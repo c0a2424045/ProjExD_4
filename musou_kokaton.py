0d8ae6f39c4e29426d2a2d3fb4c2fe211bbeab00
c0c0240101f <c0c240101f@edu.teu.ac.jp>
2025-07-01 16:45:24 +0900

    追加機能１実装完了

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -90,7 +90,7 @@ class Bird(pg.sprite.Sprite):
         引数2 screen：画面Surface
         """
         sum_mv = [0, 0]
-        if key_lst[pg.K_LSHIFT]:
+        if  key_lst[pg.K_LSHIFT]:
             self.speed = 20
         else:
             self.speed = 10
